cbc7b5de4c7c1194b5c883d0ea504ebba3f7d1c9
krumple <dmytrogl@hotmail.com>
2016-05-04 13:22:31 +0300

    root commit

---
+++ GlobalLogicTeatTask/Common/FolderOperation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+using System.IO.Compression;
+namespace GlobalLogicTeatTask.Common
+{
+    public static class FolderOperation
+    {
+        public static void MoveFolder(string startPath, string extractPath)
+        {
+            string tempPath = Path.GetTempPath() + Guid.NewGuid() + "MovingFolder";
+            ZipFile.CreateFromDirectory(startPath, tempPath, CompressionLevel.Fastest, true);
+            ZipFile.ExtractToDirectory(tempPath, extractPath);
+        }
+    }
+}
---
+++ GlobalLogicTeatTask/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using GlobalLogicTeatTask.Common;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GlobalLogicTeatTask.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Move(string startPath, string extractPath)
+        {
+            try
+            {
+               FolderOperation.MoveFolder(startPath, extractPath);
+                ViewBag.Message = "Completed!!!";
+                return View();
+            }
+            catch (IOException exp)
+            {
+                ViewBag.Message = exp.Message;
+                return View();
+            }
+            catch 
+            {
+                ViewBag.Message = "Exception occured!!!";
+                return View();
+            }
+        }
+
+    }
+}
